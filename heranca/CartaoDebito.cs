e02829809aee803b05ec2dec641a720edaa32df6
Vinicius <143036118+ViniciusCampas@users.noreply.github.com>
2025-09-16 00:10:16 +0000

    Fear: classe cartao

---
+++ heranca/CartaoDebito.cs
@@ -0,0 +1,23 @@
+
+public class CartaoDebito
+{
+
+    public CartaoDebito()
+    {
+        Bandeira = BandeiraCartao.Visa;
+    }
+
+    public string numero { get; set; }
+    //public string BandeiraCartao{get;set;}
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
+}
+
+public enum BandeiraCartao{
+    Visa,
+    Mastercard,
+    Amex,
+    Elo
+}
